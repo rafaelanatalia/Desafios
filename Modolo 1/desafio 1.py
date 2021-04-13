c22d3048eebdb2e64cb4914cb8928be2ce628756
rafaelanatalia <80581589+rafaelanatalia@users.noreply.github.com>
2021-04-13 14:51:25 -0300

    desafio

--- Modolo 1/desafio 1.py
+++ Modolo 1/desafio 1.py
@@ -2,6 +2,6 @@
 N = int(input('Digite um numero:'))
 A = N-1
 S = N+2
-print ('Analizando o valro {}, seu antecessor e {} e o sucessor é {}'.format(N,A,S))
+print ('Analizando o valor {}, seu antecessor e {} e o sucessor é {}'.format(N,A,S))
 
 
